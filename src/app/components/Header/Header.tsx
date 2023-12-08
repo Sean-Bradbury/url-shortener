9462b475ae3b09c07f898498d8153afb81cd02c0
REMARKABLE\Sean.Bradbury <sean.bradbury@remarkable.net>
2023-12-08 12:32:32 +0000

    responsiveness of nav

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Image from 'next/image'
 import Navigation from '../Navigation'
+import Icon from '../Icon'
 
 const Header = () => {
   return (
-    <div className='d-flex'>
+    <div className='flex relative w-11/12'>
         <Image src='/img/svg/logo.svg' alt='logo' width={128} height={77} />
         <Navigation />
+        <div className='absolute right-0 top-0 lg:hidden'>
+            <Icon type='hamburger' />
+        </div>
     </div>
   )
 }
--- src/app/components/Navigation/Navigation.tsx
+++ src/app/components/Navigation/Navigation.tsx
@@ -7,11 +7,11 @@ interface Props {
 
 const Navigation = ({isMobile = true, isOpen = true}: Props) => {
   return (
-    <div className={`nav flex ${!isMobile ? 'lg:flex-row ' : null } ${isMobile && isOpen ? 'flex-col bg-purple p-10 rounded-lg absolute w-4/5 left-[10%]' : 'hidden'}`}>
+    <div className={`nav ${!isMobile  ? 'lg:flex lg:flex-row gap-10 ml-10 flex-grow' : null } ${isMobile && isOpen ? 'mobile flex-col bg-purple p-10 rounded-lg absolute w-full top-20' : isMobile && !isOpen ? 'hidden' : 'flex'}`}>
         <div className="nav-link">Features</div>
         <div className="nav-link">Pricing</div>
         <div className="nav-link">Resources</div>
-        <div className="nav-link">Login</div>
+        <div className="nav-link justify-self-center ml-auto">Login</div>
         <div className="nav-link"><button className={`primary-btn ${isMobile ? 'w-100' : null}`}>Sign Up</button></div>
     </div>
   )
