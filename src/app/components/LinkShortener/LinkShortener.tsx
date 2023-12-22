a6774e5e3407207f09d9ed621ccdce082976bcf6
REMARKABLE\Sean.Bradbury <sean.bradbury@remarkable.net>
2023-12-22 09:04:17 +0000

    link shortener bgs added

--- src/app/components/LinkShortener/LinkShortener.tsx
+++ src/app/components/LinkShortener/LinkShortener.tsx
@@ -1,12 +1,11 @@
-'use client'
 import React from 'react'
-import Image from 'next/image'
+import './link-shortener.scss'
 
 const LinkShortener = () => {
 
   return (
     <section className='link-shortener bg-gray-light mt-36 px-10'>
-        <form className='link-shortener-form bg-purple rounded-xl flex flex-col gap-5 p-5 lg:flex-row lg:p-10 -translate-y-1/2'>  
+        <form className='link-shortener-form bg-purple rounded-xl flex flex-col gap-5 p-5 lg:flex-row lg:p-10 -translate-y-1/2'> 
           <input type='text' placeholder='Shorten a link here...' className='rounded py-4 px-5 lg:flex lg:flex-grow lg:w-100'/>
           <button className='primary-btn rounded'>
             Shorten It!
